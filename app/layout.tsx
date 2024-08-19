69941da99a1cbb340ac1d89b6cedd05b62aeca5d
Bryan Aldrin Quinalayo <bryandummy01@gmail.com>
2024-08-19 22:54:08 +0800

    feat(style): init shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,17 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "HRIS by 3.28",
   description: "HRIS SaaS"
 };
+
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans"
+})
 
 export default function RootLayout({
   children
@@ -18,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en' data-theme='dark'>
       <UserProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>{children}</body>
       </UserProvider>
     </html>
   );
